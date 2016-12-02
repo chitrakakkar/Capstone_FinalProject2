12ff924811a01335ff0e8501293d69cd6d34cb96
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-12-02 09:14:47 -0600

    license for face.py updated

--- Face_Recognition/FaceApp/views.py
+++ Face_Recognition/FaceApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from Face_Recognition.API_Tools.face_API import detect_face
 from . models import Image_model as mod
 # Create your views here.
 
@@ -9,9 +11,7 @@ def index(request):
     # ORM queries the database for all of the to-do entries.
     if request.method == 'GET':
         return render(request, 'FaceApp/index.html')
-    # elif request.method == "POST":
-    #     image_data = open("/Users/chitrakakkar/PycharmProjects/Capstone_FinalProject2/"
-    #                       "Face_Recognition/FaceApp/static/test.jpg", "rb").read()
-    #     return HttpResponse(image_data, content_type="image/png")
-    #
+    elif request.method =="POST":
+        Image_to_eval = request.POST.get('Image')
+        number_f_image_data = detect_face(Image_to_eval)
 
